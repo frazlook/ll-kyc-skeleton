8dee6a164547daf187f08a25d192dce70d6b31cd
Ubuntu <ubuntu@ip-172-31-28-135.us-west-2.compute.internal>
2018-12-04 14:23:50 +0000

    Add Register.css and update index.jsx for Register

--- src/components/Register/index.jsx
+++ src/components/Register/index.jsx
@@ -1,4 +1,8 @@
 import React, { Component } from 'react';
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import './Register.css'
+import "react-tabs/style/react-tabs.css";
+
 
 class Register extends Component {
   constructor(props) {
@@ -23,21 +27,37 @@ class Register extends Component {
 
   render() {
     return (
-      <form>
+
+        <div>
+          <Tabs>
+          <TabList>
+            <Tab>
+              Step 1
+            </Tab>
+            <Tab>
+              Step 2
+            </Tab>
+            <Tab>
+              Step 3
+            </Tab>
+            <Tab>
+              Step 4
+            </Tab>
+
+          </TabList>
+      
+          <TabPanel>
+            <h2>Please enter your information below:</h2>
+            <form>
             <label for="firstName"><b>First Name</b></label>
             <input type="text" id="firstName" placeholder="Enter your first name"/>
             <label for="lastName"><b>Last Name</b></label>
             <input type="text" id="lastName" placeholder="Enter your last name"/>
             <label for="email"><b>Email</b></label>
             <input type="text" id="email" value="" disabled/>
-            <label for="gender"><b>Gender</b></label>
-            <select id="gender">
-                <option value="m">Male</option>
-                <option value="f">Female</option>
-            </select>
             <label for="address"><b>Address</b></label>
             <input type="text" id="address" placeholder="Enter your address"/>
-            <label for="address2"><b>Another Address</b></label>
+            <label for="address2"><b>Address 2</b></label>
             <input type="text" id="address2" placeholder="Enter your address"/>
             <label for="city"><b>City</b></label>
             <input type="text" id="city" placeholder="Enter your city"/>
@@ -49,11 +69,11 @@ class Register extends Component {
             <input type="text" id="country" placeholder="Enter your country"/>
             <label for="idType"><b>Type of ID</b></label>
             <input type="text" id="idType" placeholder="Enter your type of ID"/>
-            <label for="phone"><b>Phone</b></label>
+            <label for="phone"><b>Home Phone</b></label>
             <input type="text" id="phone" placeholder="Enter your phone"/>
             <label for="mobile"><b>Mobile</b></label>
             <input type="text" id="mobile" placeholder="Enter your mobile"/>
-            <label for="dateOfBorn"><b>Date of born</b></label>
+            <label for="dateOfBorn"><b>Date of Birth</b></label>
             <input type="date" id="dateOfBorn" placeholder="Enter your date of born"/>
             <label for="occupation"><b>Occupation</b></label>
             <input type="text" id="occupation" placeholder="Enter your occupation"/>
@@ -62,6 +82,19 @@ class Register extends Component {
 
             <button type="button" id="submit-form-register">Submit</button>
         </form>
+          </TabPanel>
+          <TabPanel>
+            <h2>Any content 2</h2>
+          </TabPanel>
+          <TabPanel>
+            <h2>Any content 2</h2>
+          </TabPanel>
+          <TabPanel>
+            <h2>Any content 2</h2>
+          </TabPanel>
+        </Tabs>
+        </div>
+
     );
   }
 }
