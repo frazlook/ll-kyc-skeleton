e04f57e73eff5c56f02654dbd5655a94b70a6c02
Ubuntu <ubuntu@ip-172-31-28-135.us-west-2.compute.internal>
2018-12-05 14:39:59 +0000

    Updating register page

--- src/components/Register/index.jsx
+++ src/components/Register/index.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import { Form, Text } from 'informed';
 import './Register.css'
 import "react-tabs/style/react-tabs.css";
 
@@ -48,40 +49,13 @@ class Register extends Component {
       
           <TabPanel>
             <h2>Please enter your information below:</h2>
-            <form>
-            <label for="firstName"><b>First Name</b></label>
-            <input type="text" id="firstName" placeholder="Enter your first name"/>
-            <label for="lastName"><b>Last Name</b></label>
-            <input type="text" id="lastName" placeholder="Enter your last name"/>
-            <label for="email"><b>Email</b></label>
-            <input type="text" id="email" value="" disabled/>
-            <label for="address"><b>Address</b></label>
-            <input type="text" id="address" placeholder="Enter your address"/>
-            <label for="address2"><b>Address 2</b></label>
-            <input type="text" id="address2" placeholder="Enter your address"/>
-            <label for="city"><b>City</b></label>
-            <input type="text" id="city" placeholder="Enter your city"/>
-            <label for="zipCode"><b>Zip Code</b></label>
-            <input type="text" id="zipCode" placeholder="Enter your zip code"/>
-            <label for="regione"><b>State/Region</b></label>
-            <input type="text" id="regione" placeholder="Enter your state or region"/>
-            <label for="country"><b>Country</b></label>
-            <input type="text" id="country" placeholder="Enter your country"/>
-            <label for="idType"><b>Type of ID</b></label>
-            <input type="text" id="idType" placeholder="Enter your type of ID"/>
-            <label for="phone"><b>Home Phone</b></label>
-            <input type="text" id="phone" placeholder="Enter your phone"/>
-            <label for="mobile"><b>Mobile</b></label>
-            <input type="text" id="mobile" placeholder="Enter your mobile"/>
-            <label for="dateOfBorn"><b>Date of Birth</b></label>
-            <input type="date" id="dateOfBorn" placeholder="Enter your date of born"/>
-            <label for="occupation"><b>Occupation</b></label>
-            <input type="text" id="occupation" placeholder="Enter your occupation"/>
-            <label for="investmentAmount"><b>Investment Amount</b></label>
-            <input type="text" id="investmentAmount" placeholder="Enter your investment amount" />
-
-            <button type="button" id="submit-form-register">Submit</button>
-        </form>
+            <Form id="simple-form">
+  <label htmlFor="name-field">First name:</label>
+  <Text field="name" id="name-field" />
+  <button type="submit">
+    Submit
+  </button>
+</Form>
           </TabPanel>
           <TabPanel>
             <h2>Any content 2</h2>
